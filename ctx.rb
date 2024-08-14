3c683c5ac9b6829460aac4f886d69c56b669b486
Dong Yuwei <newdongyuwei@gmail.com>
2024-08-14 11:00:31 +0800

    publish as brew package, update url and sha256

--- ctx.rb
+++ ctx.rb
@@ -1,8 +1,8 @@
 class RepoContext < Formula
     desc "DConcat all source code in one git repo into one file"
     homepage "https://github.com/dongyuwei/ctx"
-    url "https://github.com/dongyuwei/ctx/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "c836fb6dc88232f38ef7fae855d89998fc69b382243b1c74df5d60a8e05dbadb"
+    url "https://github.com/dongyuwei/ctx/archive/refs/tags/v1.0.1.tar.gz"
+    sha256 "0dd6fd9130c36fc9998cd58da89b6d34f3e493395fa60f7ee4ea6f636c5d9afe"
   
     def install
       bin.install "ctx.sh" => "ctx"
